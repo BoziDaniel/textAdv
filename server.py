937c8cf6841bc3f54c6897fc621b2ca64fbfbe92
JenesesD <jenesesd@gmail.com>
2019-10-08 09:58:25 +0200

    created basic main menu

--- server.py
+++ server.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def route_main():
     return render_template ("main.html")
 
-
+@app.route('/game')
+def route_play_game():
+    return render_template('game.html')
 
 if __name__ == '__main__':
     app.run(
