e6b8074570c715e85d63c1198da7c0d9d1309db2
vutengklanricsi <richard.vuongtanquang@gmail.com>
2019-10-08 11:57:13 +0200

    bckgrnd image works

--- static/game.js
+++ static/game.js
@@ -1,9 +1,10 @@
 function $(id) {
     return document.getElementById(id);
 }
-
+/*
 $(menu).addEventListener('click', play, false);
 
 function play() {
-    document.body.style.backgroundImage = "../../pictures/starting_page.png";
+    document.body.style.backgroundImage = "../pictures/starting_page.png";
 }
+*/
