22361919a334887f69d1939aa31be486e92881a4
vutengklanricsi <richard.vuongtanquang@gmail.com>
2019-10-08 14:27:01 +0200

    display_mountain

--- static/game.js
+++ static/game.js
@@ -1,10 +1,27 @@
 function $(id) {
     return document.getElementById(id);
 }
-/*
-$(menu).addEventListener('click', play, false);
+// $ = document.getElementbyId(id)
 
-function play() {
-    document.body.style.backgroundImage = "../pictures/starting_page.png";
-}
-*/
+$('mountain').addEventListener("click", mountain, false);
+
+function mountain(){
+    document.body.style.backgroundImage = "url('../static/pictures/mountain.jpg')";
+    debugger;
+    $('story_text').innerText = "You arrive at the foot of the mountain, you see a single road leading up," +
+        " next to the road you see sign but you can’t quite make it out yet.\n";
+    $('mountain').innerText = 'Try to cross the mountain.';
+    let read_sign = document.createElement("button");
+    read_sign.setAttribute("id", 'read_sign');
+    let message = document.createTextNode("Read the sign");
+    let parent = $("start_background");
+    read_sign.appendChild(message);
+    parent.appendChild(read_sign);
+    read_sign.addEventListener("click", function (e) {
+        let alert = document.createElement("div");
+        alert.setAttribute("id", "popup");
+        alert.setAttribute("class", "alert alert-primary");
+        alert.setAttribute("role", "alert");
+        $("popup").innerText("Mount Killaraus, do not try to cross without proper preparations!");
+    });
+};
