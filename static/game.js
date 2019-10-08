871e0fbc3a054cd8829d84710a1522700f54034c
JenesesD <jenesesd@gmail.com>
2019-10-08 10:17:38 +0200

    added js file

---
+++ static/game.js
@@ -0,0 +1,10 @@
+function $(id) {
+    return document.getElementById(id);
+}
+
+
+$(menu).addEventListener('click', play, false);
+
+function play() {
+    document.body.style.backgroundImage = "../../pictures/starting_page.png";
+}
